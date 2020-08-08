09ffe1ac50c60468b55f611159ef0ce493af44b8
Zach Rickers <mzrickers@gmail.com>
2020-08-08 11:51:27 -0600

    Click-counter completion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {  
     return (
       <div data-test="component-app">
-      <h1>App</h1>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <button 
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          >
+          Increment counter
+        </button>
       </div>
     );
   }
